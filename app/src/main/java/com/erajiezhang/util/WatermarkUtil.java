a74c2f5195e9420600507e78b888c40dd792a977
EraJieZhang <1143114725@qq.com>
2019-05-18 15:23:57 +0800

    新增Rxcamera模块

--- app/src/main/java/com/erajiezhang/util/WatermarkUtil.java
+++ app/src/main/java/com/erajiezhang/util/WatermarkUtil.java
@@ -6,6 +6,9 @@ import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.util.Log;
+
+import com.erajie.rxutils.RxDeviceTool;
 
 /**
  * 图片添加水印的工具类
@@ -14,27 +17,28 @@ import android.graphics.Rect;
  */
 public class WatermarkUtil {
 	
-//	private static Bitmap createWaterMaskBitmap(Bitmap src, Bitmap watermark,
-//	                                            int paddingLeft, int paddingTop) {
-//		if (src == null) {
-//			return null;
-//		}
-//		int width = src.getWidth();
-//		int height = src.getHeight();
-//		//创建一个bitmap
-//		Bitmap newb = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);// 创建一个新的和SRC长度宽度一样的位图
-//		//将该图片作为画布
-//		Canvas canvas = new Canvas(newb);
-//		//在画布 0，0坐标上开始绘制原始图片
-//		canvas.drawBitmap(src, 0, 0, null);
-//		//在画布上绘制水印图片
-//		canvas.drawBitmap(watermark, paddingLeft, paddingTop, null);
-//		// 保存
+	private static Bitmap createWaterMaskBitmap(Bitmap src, Bitmap watermark,
+	                                            int paddingLeft, int paddingTop) {
+		if (src == null) {
+			return null;
+		}
+		int width = src.getWidth();
+		int height = src.getHeight();
+		//创建一个bitmap
+		Bitmap newb = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);// 创建一个新的和SRC长度宽度一样的位图
+		//将该图片作为画布
+		Canvas canvas = new Canvas(newb);
+		//在画布 0，0坐标上开始绘制原始图片
+		canvas.drawBitmap(src, 0, 0, null);
+		//在画布上绘制水印图片
+		canvas.drawBitmap(watermark, paddingLeft, paddingTop, null);
+		// 保存
 //		canvas.save(Canvas.ALL_SAVE_FLAG);
-//		// 存储
-//		canvas.restore();
-//		return newb;
-//	}
+		canvas.save();
+		// 存储
+		canvas.restore();
+		return newb;
+	}
 	
 	/**
 	 * 绘制文字到右下角
@@ -123,4 +127,62 @@ public class WatermarkUtil {
 		final float scale = context.getResources().getDisplayMetrics().density;
 		return (int) (dp * scale + 0.5f);
 	}
+	
+	/**
+	 * 给图片加水印，网上
+	 * 右下角
+	 * @param src       原图
+	 * @param watermark 水印
+	 * @return 加水印的原图
+	 */
+	public static Bitmap WaterMask(Bitmap src, Bitmap watermark,Context context) {
+		int w = src.getWidth();
+		int h = src.getHeight();
+		Log.i("WaterMask", "原图宽: "+w);
+		Log.i("WaterMask", "原图高: "+h);
+		// 设置原图的大小,根据屏幕大小缩放
+		float newWidth = RxDeviceTool.getScreenWidth(context);
+		float newHeight = h*(newWidth/w);
+		// 计算缩放比例
+		float scaleWidth = ( newWidth) / w;
+		float scaleHeight = (newHeight) / h;
+		Matrix matrix = new Matrix();
+		matrix.postScale(scaleWidth, scaleHeight);
+		src = Bitmap.createBitmap(src, 0, 0, w, h, matrix, true);
+		
+		//根据bitmap缩放水印图片
+		float w1 = w / 5;
+		float h1 = w1 / 5;
+		//获取原始水印图片的宽、高
+		int w2 = watermark.getWidth();
+		int h2 = watermark.getHeight();
+		
+		//计算缩放的比例
+		float scalewidth = ((float) w1) / w2;
+		float scaleheight = ((float) h1) / h2;
+		
+		Matrix matrix1 = new Matrix();
+		matrix1.postScale(scalewidth, scaleheight);
+//		matrix1.postScale((float) 0.4, (float) 0.4);
+		
+		watermark = Bitmap.createBitmap(watermark, 0, 0, w2, h2, matrix1, true);
+		//获取新的水印图片的宽、高
+		w2 = watermark.getWidth();
+		h2 = watermark.getHeight();
+		
+		Bitmap result = Bitmap.createBitmap(src.getWidth(), src.getHeight(), Bitmap.Config.ARGB_8888);
+		// 创建一个新的和SRC长度宽度一样的位图
+		Canvas cv = new Canvas(result);
+		//在canvas上绘制原图和新的水印图
+		cv.drawBitmap(src, 0, 0, null);
+		//水印图绘制在画布的右下角，距离右边和底部都为20
+		cv.drawBitmap(watermark, src.getWidth() - w2-20, src.getHeight() - h2-20, null);
+		cv.save();
+		cv.restore();
+		
+		return result;
+	}
+
+
+
 }
